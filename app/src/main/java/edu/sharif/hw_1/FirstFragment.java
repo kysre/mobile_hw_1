e1bba0d14011922111200efbd632019f02320bfa
hm3623851 <hm3623851@gmail.com>
2022-03-15 12:51:24 +0330

    professor register

--- app/src/main/java/edu/sharif/hw_1/FirstFragment.java
+++ app/src/main/java/edu/sharif/hw_1/FirstFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -29,11 +32,17 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Switch isProfessor = view.findViewById(R.id.isProfessor);
+
         binding.RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                if(isProfessor.isChecked()) {
+                    NavHostFragment.findNavController(FirstFragment.this)
+                            .navigate(R.id.action_FirstFragment_to_registerProfessor);
+                }else{
+
+                }
             }
         });
     }
