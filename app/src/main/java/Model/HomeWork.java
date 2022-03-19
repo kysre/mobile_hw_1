324d0b7ba21fef56036e2d2d6e02c5ab977b52fe
Amirreza Ghadyani <amirreza8010@gmail.com>
2022-03-19 17:04:47 +0330

    resolve confilict

--- app/src/main/java/Model/HomeWork.java
+++ app/src/main/java/Model/HomeWork.java
@@ -2,17 +2,17 @@ package Model;
 
 import java.util.HashMap;
 
-public class HomeWork {
+public class Homework {
     private String name;
     private String question;
-    private Course homeWorkCourse;
+    private Course homeworkCourse;
     private HashMap<Student, String> studentAnswers;
     private HashMap<Student, Float> studentMarks;
 
-    public HomeWork(String name, String question, Course homeWorkCourse) {
+    public Homework(String name, String question, Course homeworkCourse) {
         this.name = name;
         this.question = question;
-        this.homeWorkCourse = homeWorkCourse;
+        this.homeworkCourse = homeworkCourse;
         this.studentAnswers = new HashMap<>();
         this.studentMarks = new HashMap<>();
     }
@@ -32,56 +32,4 @@ public class HomeWork {
     public String getStudentAnswer(Student student){
         return studentAnswers.get(student);
     }
-
-
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public Course getHomeWorkCourse() {
-//        return homeWorkCourse;
-//    }
-//
-//
-//    public void setHomeWorkCourse(Course homeWorkCourse) {
-//        this.homeWorkCourse = homeWorkCourse;
-//    }
-//
-//
-//
-//    @Override
-//    public String toString() {
-//
-//        return "HomeWork{" +
-//                "name='" + name + '\'' +
-//                ", question='" + question + '\'' +
-//                ", homeWorkCourse=" + homeWorkCourse +
-//                ", studentAnswers=" + studentAnswers +
-//                ", studentMarks=" + studentMarks +
-//                '}';
-//    }
-//
-//
-//    public HashMap<Student, Float> getStudentMarks() {
-//        return studentMarks;
-//    }
-//
-//    public HashMap<Student, String> getStudentAnswers() {
-//        return studentAnswers;
-//    }
-//
-//    public void setQuestion(String question) {
-//        this.question = question;
-//    }
-//
-//    public void setStudentAnswers(HashMap<Student, String> studentAnswers) {
-//        this.studentAnswers = studentAnswers;
-//    }
-//
-//
-//    public void setStudentMarks(HashMap<Student, Float> studentMarks) {
-//        this.studentMarks = studentMarks;
-//    }
-
-
 }
--- app/src/main/java/Model/Student.java
+++ app/src/main/java/Model/Student.java
@@ -1,8 +1,5 @@
 package Model;
 
-import androidx.annotation.NonNull;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Student extends User {
@@ -16,90 +13,44 @@ public class Student extends User {
         students.add(this);
     }
 
-//    @Override
-//    public String getUsername() {
-//        return super.getUsername();
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return super.getPassword();
-//    }
-//
-//    @Override
-//    public String getLastname() {
-//        return super.getLastname();
-//    }
-//
-//    @Override
-//    public String getFirstname() {
-//        return super.getFirstname();
-//    }
-//
-//    public String getStudentId() {
-//        return studentId;
-//    }
-//
-//    public void setStudentId(String studentId) {
-//        this.studentId = studentId;
-//    }
-//
-//
-//    @Override
-//    public void setUsername(String username) {
-//        super.setUsername(username);
-//    }
-//
-//    @NonNull
-//    @Override
-//    protected Object clone() throws CloneNotSupportedException {
-//        return super.clone();
-//    }
-//
-//    @Override
-//    public boolean checkPassword(String password) {
-//        return super.checkPassword(password);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Student{" +
-//                "studentId='" + studentId + '\'' +
-//                '}';
-//    }
-//
-//    @Override
-//    public void setPassword(String password) {
-//        super.setPassword(password);
-//    }
-//
-//
-//    @Override
-//    public void setLastname(String lastname) {
-//        super.setLastname(lastname);
-//    }
-//
-//    @Override
-//    public void setFirstname(String firstname) {
-//        super.setFirstname(firstname);
-//    }
-//
-//
-//    @Override
-//    public void addCourse(Course course) {
-//        super.addCourse(course);
-//    }
-//
-//    @Override
-//    public ArrayList<Course> getCourses() {
-//        return super.getCourses();
-//    }
-//
-//    public static ArrayList<Student> getStudents() {
-//        return students;
-//    }
-//
-//    public static void setStudents(ArrayList<Student> students) {
-//        Student.students = students;
-//    }
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public String getLastname() {
+        return super.getLastname();
+    }
+
+    @Override
+    public String getFirstname() {
+        return super.getFirstname();
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public boolean joinCourse(int courseId) {
+        Course courseToJoin = Course.getCourseById(courseId);
+        if (courseToJoin == null) {
+            return false;
+        }
+        for (Course course : getCourses()) {
+            if (course.getId() == courseId) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
