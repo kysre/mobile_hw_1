5edbe6f8aac5aee81d5eeb9dcb8f5ed92998b251
Amirreza Ghadyani <amirreza8010@gmail.com>
2022-03-19 06:34:49 +0330

    add some logical methods in model

--- app/src/main/java/Model/HomeWork.java
+++ app/src/main/java/Model/HomeWork.java
@@ -5,12 +5,14 @@ import java.util.HashMap;
 public class HomeWork {
     private String name;
     private String question;
+    private Course homeWorkCourse;
     private HashMap<Student, String> studentAnswers;
     private HashMap<Student, Float> studentMarks;
 
-    public HomeWork(String name, String question) {
+    public HomeWork(String name, String question, Course homeWorkCourse) {
         this.name = name;
         this.question = question;
+        this.homeWorkCourse = homeWorkCourse;
         this.studentAnswers = new HashMap<>();
         this.studentMarks = new HashMap<>();
     }
